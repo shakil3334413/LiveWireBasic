c0967e2c3f607d84a4baa6a17de98c368872c384
shakil3334413 <shakil3334413@gmail.com>
2023-02-26 04:07:54 +0600

    livewire turbolink

---
+++ resources/views/livewire/home.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <div class="row">
+        <div class="col-md-4 borderd p-2">
+            @livewire('support-tic');
+        </div>
+        <div class="col-md-8">
+            @livewire('comments');
+        </div>
+    </div>
+</div>
