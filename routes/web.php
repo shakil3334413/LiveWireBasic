c7799f00eb5392ce48eac92036992ae8794010cc
shakil3334413 <shakil3334413@gmail.com>
2023-02-26 23:12:22 +0600

    Middleware

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,9 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::get('/',Home::class)->name('home');
-Route::get('/login',Login::class)->name('login');
-Route::get('/register',Register::class)->name('register');
+Route::get('/',Home::class)->name('home')->middleware('auth');
+
+Route::group(['middleware'=>'guest'],function(){
+    Route::get('/login',Login::class)->name('login');
+    Route::get('/register',Register::class)->name('register');
+});
